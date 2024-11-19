b2483d1e78967074b20d9d21b453e0fc331508d6
okang1014 <okang1014@gmail.com>
2024-11-19 22:23:01 +0900

    init: Udemy react-course init

---
+++ jsx/src/index.js
@@ -0,0 +1,8 @@
+// 1) Import React and React DOM library
+import React from 'react'; // 컴포넌트 정의 및 컴포넌트 연결하는 역할을 하는 라이브러리
+import ReactDOM from 'react-dom/client';
+
+// 2) get a reference to the div with ID root
+// 3) tell react to take control of that element
+// 4) create a component
+// 5) show the component on the screen
